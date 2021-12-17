ee46b45cfad670380d14e09f24ca66aa0a20b2b2
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2021-12-17 09:50:00 +0000

    softplay unfinished

--- src/soft-play.js
+++ src/soft-play.js
@@ -10,11 +10,52 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
+const softPlayCenter = {
+  children: 0,
+  adults: 0
+}
+
+function enter(numAdults, numChildren) {
+if (numChildren > numAdults ) {
+return false 
+}
+else {
+  softPlayCenter.children += numChildren
+  softPlayCenter.adults += numAdults
+  return true
+}
+}
+
+
+
+function leave(numAdults, numChildren) {
+  if (numChildren > 0 && numAdults < 0) { //children leaving must be <= adult leaving
+    return false
+  } else if 
+    (numAdults > softPlayCenter.children) { // adults leaving must be <= the children present at center
+    return false
+  } else if 
+    (numAdults > softPlayCenter.children - softPlayCenter.adults) {
+      return false
+    }
+  else if (numchildren > softPlayCenter.children || numAdults > softPlayCenter.adults){
+    return false
+  }
+  softPlayCenter.children -= numChildren
+  softPlayCenter.adults -= numAdults
+  return true
+  
+}
+
+function occupancy() {
+  return softPlayCenter.children, softPlayCenter.adults
+  
+}
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy,
   reset: reset
 }
